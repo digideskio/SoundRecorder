dde6f62ef9bdc727114c6b49b34fb7b087dfd6d8
Andrew Gaul <andrew@gaul.org>
2016-06-30 18:10:52 -0700

    Address FindBugs violations

--- app/src/main/java/com/danielkim/soundrecorder/activities/MainActivity.java
+++ app/src/main/java/com/danielkim/soundrecorder/activities/MainActivity.java
@@ -35,8 +35,8 @@ public class MainActivity extends ActionBarActivity{
         tabs.setViewPager(pager);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Light);
         if (toolbar != null) {
+            toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Light);
             setSupportActionBar(toolbar);
         }
     }
